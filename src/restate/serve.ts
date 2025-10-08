12d2e14951a640e94148f36b7c93410cf059bbdf
Pavel Tcholakov <pavel@restate.dev>
2025-10-08 09:31:37 +0200

    Add public key

--- src/restate/serve.ts
+++ src/restate/serve.ts
@@ -1,9 +1,10 @@
 import * as restate from "@restatedev/restate-sdk/fetch";
 import { greeter } from "@/restate/greeter";
 
-// Create the Restate endpoint. 
-// Here you need to register your services
-const endpoint = restate.createEndpointHandler({ services: [greeter] });
+const endpoint = restate.createEndpointHandler({
+  services: [greeter],
+  identityKeys: ["publickeyv1_5pEm4dA2ED5hM3EBGaBSkx7wfjm1wzCkDs4rLxAror8Q"],
+});
 
 // Adapt it to Next.js route handlers
 export const serve = () => {
